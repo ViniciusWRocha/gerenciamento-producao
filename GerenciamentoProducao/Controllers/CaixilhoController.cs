c01eba45b363d2257981ec54c4b851f80455e2c5
SouRyan <ryanbrayan.fs@gmail.com>
2025-09-26 21:02:10 -0300

    Criação e desenvolvimento de CaixilhoController e CaixilhoViewModel

---
+++ GerenciamentoProducao/Controllers/CaixilhoController.cs
@@ -0,0 +1,186 @@
+﻿using GerenciamentoProducao.Interfaces;
+using GerenciamentoProducao.Models;
+using GerenciamentoProducaoo.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GerenciamentoProducaoo.Controllers
+{
+    public class CaixilhoController : Controller
+    {
+        private readonly ICaixilhoRepository _caixilhoRepository;
+        private readonly ITipoCaixilhoRepository _tipoCaixilhoRepository;
+        private readonly IFamiliaCaixilhoRepository _familiaCaixilhoRepository;
+        private readonly IObraRepository _obraRepository;
+        public CaixilhoController(ICaixilhoRepository caixilhoRepository, ITipoCaixilhoRepository tipoCaixilhoRepository, IFamiliaCaixilhoRepository familiaCaixilhoRepository, IObraRepository obraRepository)
+        {
+            _caixilhoRepository = caixilhoRepository;
+            _tipoCaixilhoRepository = tipoCaixilhoRepository;
+            _familiaCaixilhoRepository = familiaCaixilhoRepository;
+            _obraRepository = obraRepository;
+        }
+
+        private async Task<CaixilhoViewModel> CriarCaixilhoViewModel(CaixilhoViewModel? model = null)
+        {
+            var tipoCaixilhos = await _tipoCaixilhoRepository.GetAllAsync();
+            var familiaCaixilhos = await _familiaCaixilhoRepository.GetAllAsync();
+            var obras = await _obraRepository.GetAllAsync();
+            return new CaixilhoViewModel
+            {
+                IdCaixilho = model?.IdCaixilho ?? 0,
+                NomeCaixilho = model.NomeCaixilho,
+                Largura = model.Largura,
+                Altura = model.Altura,
+                Quantidade = model.Quantidade,
+                PesoUnitario = model.PesoUnitario,
+
+                ObraId = model.ObraId,
+                IdFamiliaCaixilho = model.IdFamiliaCaixilho,
+                IdTipoCaixilho = model.IdTipoCaixilho,
+                //Dropdrowns 
+                Obra = obras.Select(o => new SelectListItem
+                {
+                    Value = o.IdObra.ToString(),
+                    Text = o.Nome
+                }),
+                FamiliaCaixilho = familiaCaixilhos.Select(f => new SelectListItem
+                {
+                    Value = f.IdFamiliaCaixilho.ToString(),
+                    Text = f.DescricaoFamilia
+                }),
+                TipoCaixilho = tipoCaixilhos.Select(t => new SelectListItem
+                {
+                    Value = t.IdTipoCaixilho.ToString(),
+                    Text = t.DescricaoCaixilho
+                })
+            };
+        }
+
+
+
+
+        public IActionResult Index(int? obraId, string? search)
+        {
+            // Fazer filtro por nome da obra, familia de caixilho e tipo de caixilho
+
+            //var caixilhos = _caixilhoRepository.GetAllAsync();
+            //if(obraId.HasValue && obraId.Value > 0)
+            //{
+            //    caixilhos = caixilhos.
+            //}
+            return View();
+        }
+        public async Task<IActionResult> Create()
+        {
+            var model = await CriarCaixilhoViewModel();
+            return View(model);
+        }
+        public async Task<IActionResult> Create(CaixilhoViewModel caixilhoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Mapear ViewModel para Model
+                var caixilho = new Caixilho
+                {
+                    NomeCaixilho = caixilhoViewModel.NomeCaixilho,
+                    Largura = caixilhoViewModel.Largura,
+                    Altura = caixilhoViewModel.Altura,
+                    Quantidade = caixilhoViewModel.Quantidade,
+                    PesoUnitario = caixilhoViewModel.PesoUnitario,
+                    ObraId = caixilhoViewModel.ObraId,
+                    IdFamiliaCaixilho = caixilhoViewModel.IdFamiliaCaixilho,
+                    IdTipoCaixilho = caixilhoViewModel.IdTipoCaixilho
+                };
+                await _caixilhoRepository.AddAsync(caixilho);
+                return RedirectToAction(nameof(Index));
+            }
+            var model = await CriarCaixilhoViewModel(caixilhoViewModel);
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var caixilho = await _caixilhoRepository.GetById(id);
+            var tipoCaixilhos = await _tipoCaixilhoRepository.GetAllAsync();
+            var familiaCaixilhos = await _familiaCaixilhoRepository.GetAllAsync();
+            var obras = await _obraRepository.GetAllAsync();
+
+            if (caixilho == null) return NotFound();
+            var model = new CaixilhoViewModel
+            {
+                IdCaixilho = caixilho.IdCaixilho,
+                NomeCaixilho = caixilho.NomeCaixilho,
+                Largura = caixilho.Largura,
+                Altura = caixilho.Altura,
+                Quantidade = caixilho.Quantidade,
+                PesoUnitario = caixilho.PesoUnitario,
+
+                ObraId = caixilho.ObraId,
+                IdFamiliaCaixilho = caixilho.IdFamiliaCaixilho,
+                IdTipoCaixilho = caixilho.IdTipoCaixilho,
+                //Dropdrowns 
+                Obra = obras.Select(o => new SelectListItem
+                {
+                    Value = o.IdObra.ToString(),
+                    Text = o.Nome
+                }),
+                FamiliaCaixilho = familiaCaixilhos.Select(f => new SelectListItem
+                {
+                    Value = f.IdFamiliaCaixilho.ToString(),
+                    Text = f.DescricaoFamilia
+                }),
+                TipoCaixilho = tipoCaixilhos.Select(t => new SelectListItem
+                {
+                    Value = t.IdTipoCaixilho.ToString(),
+                    Text = t.DescricaoCaixilho
+                })
+            };
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, CaixilhoViewModel viewModel)
+        {
+            if (id != viewModel.IdCaixilho)
+            {
+                return NotFound();
+            }
+            if (!ModelState.IsValid) {
+                var caixilho = await _caixilhoRepository.GetById(id);
+                if (caixilho == null) { return NotFound(); }
+                caixilho.NomeCaixilho = viewModel.NomeCaixilho;
+                caixilho.Largura = viewModel.Largura;
+                caixilho.Altura = viewModel.Altura;
+                caixilho.Quantidade = viewModel.Quantidade;
+                caixilho.PesoUnitario = viewModel.PesoUnitario;
+                caixilho.ObraId = viewModel.ObraId;
+                caixilho.IdFamiliaCaixilho = viewModel.IdFamiliaCaixilho;
+                caixilho.IdTipoCaixilho = viewModel.IdTipoCaixilho;
+                await _caixilhoRepository.UpdateAsync(caixilho);
+                return RedirectToAction(nameof(Index));
+            }
+            //Se o modelo não estiver válido, recarrega o formulário
+            // podemos trocar para outra coisa invés disso
+            viewModel = await CriarCaixilhoViewModel(viewModel);
+            return View(viewModel);
+        }
+
+        //[Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var item = await _caixilhoRepository.GetById(id);
+            if (item == null) return NotFound();
+            return View(item);
+        }
+
+        //[HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _caixilhoRepository.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ GerenciamentoProducao/ViewModel/CaixilhoViewModel.cs
@@ -0,0 +1,27 @@
+﻿using GerenciamentoProducao.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace GerenciamentoProducaoo.ViewModel
+{
+    public class CaixilhoViewModel
+    {
+
+        public int IdCaixilho { get; set; }
+        public string NomeCaixilho { get; set; }
+        public int Largura { get; set; }
+        public int Altura { get; set; }
+        public int Quantidade { get; set; }
+        public float PesoUnitario { get; set; }
+
+        public int ObraId { get; set; }
+        public int IdFamiliaCaixilho { get; set; }
+        public int IdTipoCaixilho { get; set; }
+
+        public IEnumerable<SelectListItem> Obra { get; set; }
+        public IEnumerable<SelectListItem> FamiliaCaixilho { get; set; }
+        public IEnumerable<SelectListItem> TipoCaixilho { get; set; }
+
+
+    }
+}
