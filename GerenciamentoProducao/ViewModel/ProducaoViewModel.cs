c570811fbb8f560e7c67aeab55ba72a3812dfc91
ViniciusWRocha <viniciuswallace77@gmail.com>
2025-10-01 21:57:50 -0300

    Ajustando Edit e Criando ProducaoViewModel

---
+++ GerenciamentoProducao/ViewModel/ProducaoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using GerenciamentoProducao.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace GerenciamentoProducaoo.ViewModel
+{
+    public class ProducaoViewModel
+    {
+        public int IdProducao { get; set; }
+        public string NomeProducao { get; set; }      
+        public DateTime DataProducao { get; set; }      
+        public bool Produzido { get; set; }       
+        public string? Descricao { get; set; }
+        public bool EhLiberado { get; set; }
+
+
+        public IEnumerable<SelectListItem>? Usuario { get; set; }
+
+        public IEnumerable<SelectListItem>? FamiliaCaixilho { get; set; }
+
+     
+    }
+}
