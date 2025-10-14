4fecd9a451954ef4949813abf71fe3108ac5b37f
ViniciusWRocha <viniciuswallace77@gmail.com>
2025-10-13 22:05:46 -0300

    Criacao da API

---
+++ GerenciamentoProducao.API/Program.cs
@@ -0,0 +1,51 @@
+using GerenciamentoProducao.Data;
+using GerenciamentoProducao.Interfaces;
+using GerenciamentoProducao.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//builder.Services.AddControllers();
+
+// DB
+builder.Services.AddDbContext<GerenciamentoProdDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Repositórios
+builder.Services.AddScoped<IObraRepository, ObraRepository>();
+
+
+//configuração de CORS
+//Não esquecer de colocar enavledCors nas controllers 
+builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+}));
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//ativar o Cors
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
