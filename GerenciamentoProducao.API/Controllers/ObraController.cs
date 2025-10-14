d639a8a80edbf5b771baa7920d0ff8235084efd6
ViniciusWRocha <viniciuswallace77@gmail.com>
2025-10-13 22:05:46 -0300

    Criacao da API

---
+++ GerenciamentoProducao.API/Controllers/ObraController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using GerenciamentoProducao.Interfaces;
+
+
+namespace GerenciamentoProducao.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    //[EnableCores("MyPolicy")]
+    [ApiController]
+    public class ObraController : Controller
+    {
+        private readonly IObraRepository _obraRepository;
+        public ObraController(IObraRepository obraRepository)
+        {
+            _obraRepository = obraRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GerenciamentoProducao.API/DTOs/ObraOutputDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace GerenciamentoProducao.API.DTOs
+{
+    public class ObraOutputDTO
+    {      
+        public int IdObra { get; set; }
+        public string Nome { get; set; }
+        public string Construtora { get; set; }
+        public string Nro { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Cep { get; set; }
+        public string Uf { get; set; }
+        public string Cnpj { get; set; }
+        public int IdUsuario { get; set; }
+        public string NomeUsuario { get; set; }     
+    }
+}
