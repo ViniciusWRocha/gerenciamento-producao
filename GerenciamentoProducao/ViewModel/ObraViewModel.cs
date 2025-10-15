2d007334428956e1070bb2dec68beb3a530222f5
ViniciusWRocha <viniciuswallace77@gmail.com>
2025-10-14 21:57:09 -0300

    Correção nas Views

---
+++ GerenciamentoProducao/ViewModel/ObraViewModel.cs
@@ -0,0 +1,24 @@
+﻿using GerenciamentoProducao.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GerenciamentoProducaoo.ViewModel
+{
+    public class ObraViewModel
+    {
+      
+        public int IdObra { get; set; }
+        public string Nome { get; set; }    
+        public string Construtora { get; set; }
+        public string Nro { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Cep { get; set; }
+        public string Uf { get; set; }
+        public string Cnpj { get; set; }
+        public int IdUsuario { get; set; }
+        public IEnumerable<SelectListItem> Usuario { get; set; }
+
+    }
+}
