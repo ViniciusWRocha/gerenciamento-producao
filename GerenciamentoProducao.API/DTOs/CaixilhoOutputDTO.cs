f07eaefa9be3be56cadd8a00acade4b5c720ebe4
SouRyan <ryanbrayan.fs@gmail.com>
2025-10-17 20:41:24 -0300

    Adição de Caixilhos na API

---
+++ GerenciamentoProducao.API/DTOs/CaixilhoOutputDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace GerenciamentoProducao.API.DTOs
+{
+    public class CaixilhoOutputDTO
+    {
+        public int IdCaixilho { get; set; }
+        public string NomeCaixilho { get; set; }
+        public string Largura { get; set; }
+        public string Altura { get; set; }
+        public string Quantidade { get; set; }
+        public string PesoUnitario { get; set; }
+        public int ObraId { get; set; }
+        public string nomeObra { get; set; }
+        public int IdTipoCaixilho{ get; set; }
+        public string DescricaoCaixilho { get; set; }
+    }
+}
