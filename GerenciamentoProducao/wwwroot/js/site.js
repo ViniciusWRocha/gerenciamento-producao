5fc832bcc680b83ddacaa3903d612f37fe44cab8
SouRyan <ryanbrayan.fs@gmail.com>
2025-10-21 19:59:01 -0300

    Revert "Importação XML para todos os models"
    This reverts commit b399ee1c6dca8df1cfda906d4f9eed0ab58011a4.

--- GerenciamentoProducao/wwwroot/js/site.js
+++ GerenciamentoProducao/wwwroot/js/site.js
@@ -1,11 +1,4 @@
-﻿
-const sidebar = document.getElementById("sidebar");
-const toggleButton = document.getElementById("toggle-sidebar");
-const itens = document.getElementById("itensMenu");
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
 
-
-toggleButton.addEventListener("click", () => {
-	// Apenas alterna a classe no elemento principal
-	sidebar.classList.toggle("collapsed");
-
-});
+// Write your JavaScript code.
